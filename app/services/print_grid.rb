4106d20770a06555c4714120ddff3b709caccce7
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-02-27 00:51:15 -0500

    Fix code for when n=3

--- app/services/print_grid.rb
+++ app/services/print_grid.rb
@@ -17,7 +17,7 @@ module Services
       end
       @grid = start_grid(primes)
       # byebug
-      primes.each {|prime| append_grid(prime, _n) }
+      primes.each {|prime| append_grid(prime, _n, primes) }
       return @grid
     end
 
@@ -30,14 +30,29 @@ module Services
       return grid
     end
 
-    def append_grid(val, _n)
+    def append_grid(val, _n, primes)
       _val = val
+      idx = 0
       (_n+1).times do
-        @grid << "|     #{val} "
-        val += _val
+        # @grid << "|     #{val} "
+        @grid << spaced_string(val)
+        # p "#{_val}:#{idx}:#{primes[idx]}:_val*primes[idx]" if idx < primes.length
+        val = _val*primes[idx] if idx < primes.length
+        idx += 1
       end
+      # p '----'
       @grid << "|\n"
+    end
+
+    def spaced_string(val)
+      val_length = (val).to_s.length
+      space_length = (6-val_length)
+      str = ''
+      space_length.times { str << ' ' }
+      # str << (val.to_s)
+      str << "#{val} "
       # byebug
+      return "|#{str}"
     end
   end
 end
