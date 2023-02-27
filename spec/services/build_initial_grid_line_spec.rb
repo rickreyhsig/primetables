0de2f3ef171d37e72fdbe6bcd306150580acf612
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-02-27 01:36:51 -0500

    Begin further separation of concerns for building initial grid line

---
+++ spec/services/build_initial_grid_line_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Services::BuildInitialGridLine do
+  subject { described_class.new }
+
+  describe 'process' do
+    it 'when no params are provided, an error is triggered' do
+      expect { subject.process }.to raise_error(ArgumentError)
+    end
+
+    it 'returns response when primes is provided' do
+      expect(subject.process([2,3,5])).to eq("|       |     2 |     3 |     5 |\n")
+    end
+  end
+end
