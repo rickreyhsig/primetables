79d58833d5ed4090c84cec3a7465704fc2014620
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-02-26 19:47:16 -0500

    Fix code for when n=1

--- app/services/print_grid.rb
+++ app/services/print_grid.rb
@@ -1,11 +1,41 @@
 module Services
   class PrintGrid
+
+    def initialize
+      @grid = nil
+    end
+
     def process(_n)
       raise ArgumentError unless _n
-      raise 'N should be at least 1' if _n < 1
-      return '|       |' if _n == 1
+
+      m = 0
+      incr = 0
+      while(m < _n)
+        primes = Services::Primes.new.process(_n+incr)
+        m = primes.length
+        incr += 1
+      end
+      @grid = start_grid(primes)
+      primes.each {|prime| append_grid(prime, _n) }
+      return @grid
     end
 
     private
+
+    def start_grid(primes)
+      grid = "|       |"
+      primes.each {|prime| grid << "     #{prime} " }
+      grid << "|\n"
+      return grid
+    end
+
+    def append_grid(val, _n)
+      (_n+1).times do
+        @grid << "|     #{val} "
+        val += val
+      end
+      @grid << "|\n"
+      # byebug
+    end
   end
 end
--- spec/services/print_grid_spec.rb
+++ spec/services/print_grid_spec.rb
@@ -4,16 +4,14 @@ describe Services::PrintGrid do
   subject { described_class.new }
 
   describe 'process' do
+    let(:n) { nil }
     it 'when no params are provided, an error is triggered' do
       expect { subject.process }.to raise_error(ArgumentError)
     end
 
-    it 'when n is less than one' do
-      expect { subject.process(0) }.to raise_error('N should be at least 1')
-    end
-
     it 'when n is 1' do
-      grid = "|       |"
+      grid = "|       |     2 |\n"\
+             "|     2 |     4 |\n"
       expect(subject.process(1)).to eq(grid)
     end
   end
